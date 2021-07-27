c132110d11963cd513a3dd53e0b4106b452d7bbb
Keshari <butterflykiki05@gmail.com>
2021-07-27 14:47:23 -0400

    added dashboard and single student route

--- server.js
+++ server.js
@@ -21,9 +21,16 @@ app.get('/profile', function (req, res) {
 // admin registration route (antony)
 
 // admin dash route (keshari)
+app.get('/admin-dash', function (req, res) {
+  console.log("you hit the admin dash route");
+  res.send('admin dash page here')
+})
 
 // admin single student route (keshari)
-
+app.get('/single-student', function (req, res) {
+  console.log("you hit the single-student route");
+  res.send('admin student view page here')
+})
 
 
  
