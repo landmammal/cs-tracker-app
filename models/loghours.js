02adeb8477df215a3a1868393f8b04f5b56eab6e
Keshari <butterflykiki05@gmail.com>
2021-08-02 16:06:49 -0400

    added log hours table and packages

---
+++ models/loghours.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const bcrypt = require("bcryptjs");
+
+module.exports = (sequelize, DataTypes) => {
+ const LogHours = sequelize.define("LogHours", {
+      ID: DataTypes.INTEGER,
+      date: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      location: DataTypes.STRING,
+      contact: DataTypes.STRING,
+      description: DataTypes.STRING,
+      start_time: DataTypes.INTEGER,
+      end_time: DataTypes.INTEGER,
+      total_hours: DataTypes.INTEGER,
+      approved: DataTypes.BOOLEAN,
+      student_id: DataTypes.INTEGER,
+  });
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+
+  return LogHours;
+};
