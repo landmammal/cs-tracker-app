536f5a2d81911ccfc14d43fcf68392e53997116d
landmammal <evolpirate@gmail.com>
2021-08-02 11:33:22 -0500

    adding db schema

---
+++ database/schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE CS_Tracker;
+
+USE CS_Tracker;
+
+CREATE TABLE Users(
+	ID int,
+	name varchar(255),
+	email varchar(255),
+	password varchar(255),
+	pfp varchar(255),
+	school varchar(255),
+	hours_needed int,
+	done_hours int,
+	admin bool,
+	student_id int
+);
+
+Create Table loghours (
+  ID int,
+  date timestamp,
+  name varchar(255),
+  location varchar(255),
+  contact varchar(255),
+  description text,
+  start_time timestamp,
+  end_time timestamp,
+  total_hours float,
+  approved bool,
+  student_id int
+  );
+  
+  DROP DATABASE CS_Tracker;
